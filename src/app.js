a3cc74a67cb2decb9766b9541a0d81a2a3baa827
Taynara Pecorario <taynara_rp22@hotmail.com>
2023-04-10 11:03:50 -0300

    :sparkles: adding  post and get tweets

--- src/app.js
+++ src/app.js
@@ -8,6 +8,12 @@ app.use(express.json());
 const users = [];
 const tweets = [];
 
+const findUser = username => {
+  return users.find(
+    user => user.username.toLowerCase() === username.toLowerCase()
+  );
+};
+
 app.post('/sign-up', (req, res) => {
   const { username, avatar } = req.body;
 
@@ -19,20 +25,57 @@ app.post('/sign-up', (req, res) => {
   if (avatar === '' || avatar === undefined) {
     errors.push('O avatar é obrigatório!');
   }
+  if (findUser(username)) {
+    errors.push('Este username já está em uso!');
+  }
 
   if (errors.length > 0) {
     return res.status(422).send(errors);
-  }
-
-  if (
-    username !== '' &&
-    avatar !== '' &&
-    avatar !== undefined &&
-    username !== undefined
-  ) {
+  } else {
     users.push({ username, avatar });
     return res.status(200).send('Ok');
   }
+});
+
+app.post('/tweets', (req, res) => {
+  const { user } = req.headers;
+  const { username, tweet } = req.body;
+
+  const errors = [];
+
+  if (!findUser(user)) {
+    return res.status(401).send('UNAUTHORIZED');
+  }
+
+  if (username === '' || username === undefined) {
+    errors.push('O campo username é obrigatório!');
+  }
+  if (tweet === '' || tweet === undefined) {
+    errors.push('O campo tweet é obrigatório!');
+  }
+
+  if (errors.length > 0) {
+    return res.status(422).send(errors);
+  } else {
+    tweets.push({ username, tweet });
+    return res.status(200).send('Ok');
+  }
+});
+
+app.get('/tweets', (req, res) => {
+  const idxLastTenTweets = tweets.length - 11;
+  console.log('idxLastTenTweets', idxLastTenTweets);
+  console.log('tweets.length', tweets.length);
+
+  const lastTenTweets = tweets
+    .filter((_, idx) => idx > idxLastTenTweets)
+    .map(item => {
+      const avatar = findUser(item.username).avatar;
+
+      return { ...item, avatar };
+    });
+
+  return res.status(200).send(lastTenTweets);
 });
 
 const PORT = 5000;
